f6cac7db7b2d65ac01f85081e119c4ae83cf10f4
ProcGenStyle <170687727+ProcGenStyle@users.noreply.github.com>
2024-12-02 21:48:46 -0500

    Day02

--- AdventOfCode2024/Day02/Program.cs
+++ AdventOfCode2024/Day02/Program.cs
@@ -4,6 +4,7 @@ using Common;
 // Puzzle answers
 var safeReports = 0;
 var dampenedReports = 0;
+var itemRemoved = false;
 
 // Read in each report
 var reports = FileParser.ReadAllLinesAsIntArrays("input.txt");
@@ -15,56 +16,68 @@ foreach (var report in reports)
     var deltas = new List<int>();
     var dirs = new List<char>();
 
-    // Markers for any invalid levels
-    var invDelta = -1;
-    var invDir = -1;
-
     // Compute the difference and direction for each element
     for (int i = 1; i < report.Length; i++)
     {
-        var delta = Math.Abs(report[i] - report[i - 1]);
-        var dir = (report[i] - report[i - 1]) > 0 ? '+' : '-';
-
-        deltas.Add(delta);
-        dirs.Add(dir);
-
-        if (delta > 3 || delta < 1)
-            invDelta = i;
-        if (dir != dirs[0])
-            invDir = i;
+        var delta = report[i] - report[i - 1];
+        deltas.Add(Math.Abs(delta));
+        dirs.Add(delta > 0 ? '+' : delta < 0 ? '-' : '0');
     }
 
     // Check to see if this report is safe
-    if (invDelta == -1 && invDir == -1)
+    if (deltas.All(d => d <= 3 && d >= 1) && dirs.All(d => d == dirs[0]))
         safeReports += 1;
 
-    // Try removing one invalid level
-    if (invDelta > 0)
-    {
-        deltas.RemoveAt(invDelta);
-        dirs.RemoveAt(invDelta);
-    }
-    else if (invDir > 0)
-    {
-        deltas.RemoveAt(invDir);
-        dirs.RemoveAt(invDir);
-    }
-
-    // Check to see if the report is safe after removing a level
-    var safeDelta = deltas.All(d => d <= 3 && d >= 1);
-    var safeDir = dirs.All(d => d == dirs[0]);
-    
-    // Count the report if it's safe
-    if (safeDelta && safeDir)
-        dampenedReports += 1;
-
     // Debug
-    //Console.WriteLine($"{string.Join(' ', report)}");
-    //Console.WriteLine($"  {string.Join(' ', deltas)}");
-    //Console.WriteLine($"  {string.Join(' ', dirs)}");
-    //Console.WriteLine($"  {invDelta} {invDir}");
-    //Console.WriteLine();
+    Console.WriteLine($"{string.Join('\t', report)}");
+    Console.WriteLine($"\t{string.Join('\t', deltas)}");
+    Console.WriteLine($"\t{string.Join('\t', dirs)}");
+
+    // Try removing one invalid level
+    for (int i = deltas.Count - 1; i >= 0; i--)
+    {
+        if (deltas[i] > 3 || deltas[i] < 1)
+        {
+            Console.WriteLine($"Removed {deltas[i]} {dirs[i]}");
+            deltas.RemoveAt(i);
+            dirs.RemoveAt(i);
+            itemRemoved = true;
+            break;
+        }
+    }
+
+    // Check to see if a dir needs to be removed
+    var tot = dirs.Count();
+    var plus = dirs.Count(c => c == '+');
+    var minus = dirs.Count(c => c == '-');
+    if (plus > minus && plus != tot && !itemRemoved)
+    {
+        var i = dirs.IndexOf('-');
+        Console.WriteLine($"Removed {deltas[i]} {dirs[i]}");
+        deltas.RemoveAt(i);
+        dirs.RemoveAt(i);
+    }
+    else if (minus > plus && minus != tot && !itemRemoved)
+    {
+        var i = dirs.IndexOf('+');
+        Console.WriteLine($"Removed {deltas[i]} {dirs[i]}");
+        deltas.RemoveAt(i);
+        dirs.RemoveAt(i);
+    }
+
+    // Check again to see if this report is safe after removing a level
+    if (deltas.All(d => d <= 3 && d >= 1) && dirs.All(d => d == dirs[0]))
+    {
+        dampenedReports += 1;
+        Console.WriteLine("Safe");
+    }
+    else { Console.WriteLine("Unsafe"); }
+    Console.WriteLine();
+
+    // Reset removed flag for next cycle
+    itemRemoved = false;
 }
 
+// Print results
 Console.WriteLine(safeReports);
 Console.WriteLine(dampenedReports);
